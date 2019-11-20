2d124d8a6538ddc18c2529a6b28473765f46143a
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-11-20 08:57:41 -0500

    removed theme settings

--- splash_screen.py
+++ splash_screen.py
@@ -1,7 +1,7 @@
 """
     Sample Splash Screen with images and loading progress bar
     Author      :   Israel Dryer
-    Modified    :   2019-11-19
+    Modified    :   2019-11-20
 """
 import PySimpleGUI as sg
 from threading import Thread
@@ -19,18 +19,6 @@ messages = iter([
     'programming destination coordinates...',
     'setting sail for an adventure!!!' ])
 
-sg.LOOK_AND_FEEL_TABLE['custom'] = {
-        'BACKGROUND': '#E8E8E8',
-        'TEXT': '#234988',
-        'INPUT': '#000000',
-        'TEXT_INPUT': '#000000',
-        'SCROLL': '#000000',
-        'BUTTON': '#000000',
-        'PROGRESS': ('#199FD0', '#FFFFFF'),
-        'PROGRESS_DEPTH': 0,
-        'BORDER': 1,
-        'SLIDER_DEPTH': 0 }
-
 # --- SOME SIMULATED PROCESS -------------------------------------------------
 bar_count = 0
 active = True
@@ -45,14 +33,14 @@ def some_external_process():
 
 # --- GUI --------------------------------------------------------------------
 def splash_gui():
-    sg.change_look_and_feel('custom')
     layout = [
-        [sg.Text('Splash Screen', justification='center', font=('Tahoma', 30), pad=((5, 5), (25, 5)))],
+        [sg.Text('Splash Screen', justification='center', text_color='#234988', font=('Tahoma', 30), pad=((5, 5), (25, 5)))],
         [sg.Text('Splash Screen Demo for Your Program', justification='center', size=(50, 1), 
-                font=('Tahoma', 10), pad=((5, 5), (10, 25)), key='MSG')],
-        [sg.Text('0%', size=(5, 1), font=('Tahoma', 12), pad=((5, 5), (5, 12)), key='PCT')],
+                text_color='#234988', font=('Tahoma', 10), pad=((5, 5), (10, 25)), key='MSG')],
+        [sg.Text('0%', size=(5, 1), text_color='#234988', font=('Tahoma', 12), pad=((5, 5), (5, 12)), key='PCT')],
         [sg.Image('', key='BOAT')],
-        [sg.ProgressBar(max_value=100, orientation='h', border_width=1, size=(25, 25), key='PRG')],
+        [sg.ProgressBar(max_value=100, orientation='h', border_width=1, size=(25, 25), 
+                        bar_color=('#199FD0', '#FFFFFF'), key='PRG')],
         [sg.Image(filename='images/water-small.png', key='WATER')]]
 
     return sg.Window('splash', layout, no_titlebar=True, element_justification='center', 
